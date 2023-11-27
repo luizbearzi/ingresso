b24e3c90faddf0d41752630c07c3cdf500abebc7
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-11-27 00:16:12 -0300

    Recuperando informações do formulário

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function comprar() {
+    //recuperar informações do formulando
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidadeIngresso = document.getElementById('qtd').value;
+
+    
+    let qtdIngressoPista = document.getElementById('qtd-pista').textContent;
+    alert(qtdIngressoPista);
+    let qtdIngressoSuperior = document.getElementById('qtd-superior').textContent;
+    alert(qtdIngressoSuperior);
+    let qtdIngressoInferior = document.getElementById('qtd-inferior').textContent;
+    alert(qtdIngressoInferior);
+
+
+    // fazendo cálculo descontando os ingressos
+    
+}
