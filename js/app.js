9688637b93b4925bc19b27ec9e26e451b0b863c3
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-11-28 00:46:06 -0300

    Cálculos descontando ingresso usando condicionais e funções

--- js/app.js
+++ js/app.js
@@ -1,17 +1,55 @@
 function comprar() {
-    //recuperar informações do formulando
-    let tipoIngresso = document.getElementById('tipo-ingresso').value;
-    let quantidadeIngresso = document.getElementById('qtd').value;
+    //recuperar informações do formulario
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let quantidadeIngresso = parseInt(document.getElementById('qtd').value);
 
-    
-    let qtdIngressoPista = document.getElementById('qtd-pista').textContent;
-    alert(qtdIngressoPista);
-    let qtdIngressoSuperior = document.getElementById('qtd-superior').textContent;
-    alert(qtdIngressoSuperior);
-    let qtdIngressoInferior = document.getElementById('qtd-inferior').textContent;
-    alert(qtdIngressoInferior);
+    // fazendo cálculo descontando os ingressos 
+    if (tipoIngresso.value == 'pista') {
+        comprarPista(quantidadeIngresso);
+    } 
+    if (tipoIngresso.value == 'superior') {
+        comprarSuperior(quantidadeIngresso);
+    }
+    if (tipoIngresso.value == 'inferior') {
+        comprarInferior(quantidadeIngresso);
+    }
 
+}
 
-    // fazendo cálculo descontando os ingressos
+function comprarPista(quantidadeIngresso) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (quantidadeIngresso > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - quantidadeIngresso;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
     
 }
+
+function comprarSuperior(quantidadeIngresso) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (quantidadeIngresso > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - quantidadeIngresso;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+    
+}
+
+function comprarInferior(quantidadeIngresso) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (quantidadeIngresso > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        qtdInferior = qtdInferior - quantidadeIngresso;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+    
+}
+
+
